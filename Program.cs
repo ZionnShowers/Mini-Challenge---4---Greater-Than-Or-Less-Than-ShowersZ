1bcd4dce08aec58ef65a0f4e36ea22c22e5d9c32
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-09 15:44:26 -0700

    First Version

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//Zionn Showers
+//9-9-2026
+//Mini Challenge # 4 - Greater Than Or Less Than
+//I made the user type in two different numbers and save them as strings. I then made the code convert the strings into integers. I then used a lot of else if statements to correctly calculate the integers. Afterwards, the program says if the two numbers are greater, lesser, or equal to each other
+//Peer Review:
+//Review:
+
+Console.Clear(); //Setup
+
+Console.WriteLine("Enter a number:"); //Number setups
+string num1 = Console.ReadLine();
+Console.WriteLine("Enter another number:");
+string num2 = Console.ReadLine();
+int actualNum1 = Convert.ToInt32(num1);
+int actualNum2 = Convert.ToInt32(num2);
+
+
+if(actualNum1 > actualNum2)// 1st Number Output
+{
+    Console.WriteLine($"{actualNum1} is greater than {actualNum2}!");
+}
+else if(actualNum1 < actualNum2)
+{
+    Console.WriteLine($"{actualNum1} is lesser than {actualNum2}!");
+}
+else
+{
+    Console.WriteLine($"{actualNum1} is equal to {actualNum2}!");
+}
+
+if(actualNum2 > actualNum1)// 2nd Number Output
+{
+    Console.WriteLine($"{actualNum2} is greater than {actualNum1}!");
+}
+else if(actualNum2 < actualNum1)
+{
+    Console.WriteLine($"{actualNum2} is lesser than {actualNum1}!");
+}
+else
+{
+    Console.WriteLine($"{actualNum2} is equal to {actualNum1}!");
+}
